7f86cdd29eb902d86275c0751f8b7986bb7d486a
Sonja Leaf <avleaf@gmail.com>
2017-12-31 20:41:13 -0600

    trying this and/or that

--- src/assistant_library_demo.py
+++ src/assistant_library_demo.py
@@ -84,7 +84,8 @@ def process_event(assistant, event, denon):
         elif text == 'ip address':
             assistant.stop_conversation()
             say_ip()
-        elif (text.find("xbox") == 1) or (text.find("apple") == 1) or (text.find("audio") == 1) or (text.find("video") == 1) or (text.find("music") == 1) or (text.find("stereo") == 1) or (text.find("dolby") == 1) or (text.find("dts") == 1) or (text.find("volume") == 1) or (text.find("tv") == 1):
+        elif text.find('denon') == 1:
+        # elif (text.find("xbox") == 1) or (text.find("apple") == 1) or (text.find("audio") == 1) or (text.find("video") == 1) or (text.find("music") == 1) or (text.find("stereo") == 1) or (text.find("dolby") == 1) or (text.find("dts") == 1) or (text.find("volume") == 1) or (text.find("tv") == 1):
             print("text found")
             assistant.stop_conversation()
             denon.handle_command_queue(denon.process_command(text))
